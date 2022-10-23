6450791cd0119f355f89cda0584dcd961233095e
mertece9 <112589239+mertece9@users.noreply.github.com>
2022-10-23 20:42:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ if not os.path.exists(f"Users/1955509952/phone.csv"):
 if not os.path.exists('data.csv'):
     open("data.csv","w")
 UPDATES_CHANNEL = "marvelturkey"
-OWNER= [1955509952,5588996470]
-PREMIUM=[1955509952,5588996470]
+OWNER= [641319713,5588996470]
+PREMIUM=[641319713,5588996470]
 app = pyrogram.Client("app", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 with open("data.csv", encoding='UTF-8') as f:
